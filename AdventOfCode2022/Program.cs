121f47c63ae70fbc13b40b028354704f8a8a5c7b
Kunkulis <kunkulis@inbox.lv>
2022-12-01 07:29:49 +0200

    Add project files.

---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,20 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+var results = new List<int>();
+var elf = 0;
+
+foreach (var line in input)
+{
+    if (line.Length == 0)
+    {
+        results.Add(elf);
+        elf = 0;
+    }
+    else
+    {
+        elf+=int.Parse(line);
+    }
+}
+
+Console.WriteLine(results.Max());
+Console.WriteLine(results.OrderByDescending(x=>x).Take(3).Sum());
