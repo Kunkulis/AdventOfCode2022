a3026171cda509cd8d1368cf3f487264cf0ff150
Kunkulis <kunkulis@inbox.lv>
2022-12-09 08:20:50 +0200

    day6

---
+++ Day6/Program.cs
@@ -0,0 +1,26 @@
+﻿var input = File.ReadAllText("input.txt");
+
+var position = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+    var sub = input.Substring(i, 4);
+    if (sub.Distinct().Count() == sub.Length)
+    {
+        position = i + 4;
+        break;
+    }
+}
+Console.WriteLine(position);
+
+position = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    var sub = input.Substring(i, 14);
+    if (sub.Distinct().Count() == sub.Length)
+    {
+        position = i + 14;
+        break;
+    }
+}
+Console.WriteLine(position);
